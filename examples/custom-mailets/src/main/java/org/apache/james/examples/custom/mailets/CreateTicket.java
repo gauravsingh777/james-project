ed3cbb81385eaa8748d45ba3f3862ee24520d98c
Gaurav Singh <gauravsingh@xecurify.com>
2024-04-03 13:05:21 +0530

    Added check for blocked email replies

--- examples/custom-mailets/src/main/java/org/apache/james/examples/custom/mailets/CreateTicket.java
+++ examples/custom-mailets/src/main/java/org/apache/james/examples/custom/mailets/CreateTicket.java
@@ -1,19 +1,21 @@
 package org.apache.james.examples.custom.mailets;
 
 import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.mail.util.MimeMessageParser;
 import org.apache.mailet.Mail;
 import org.apache.mailet.base.GenericMailet;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class CreateTicket extends GenericMailet {
     private static final Logger logger = LoggerFactory.getLogger(CreateTicket.class);
@@ -31,7 +33,9 @@ public class CreateTicket extends GenericMailet {
         logger.info("mail Subject: {}", mail.getMessage().getSubject());
         logger.info("mail description: {}", mail.getMessage().getDescription());
         logger.info("mail sent date: {}", mail.getMessage().getSentDate());
+
         try {
+            logger.info("mail content: {}", mail.getMessage().getContent());
             createTicket(mail);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -50,31 +54,35 @@ public class CreateTicket extends GenericMailet {
         jsonObject.put("sender", mail.getMaybeSender().asString());
         jsonObject.put("recipients", new JSONArray(mail.getRecipients().toString()));
         jsonObject.put("subject", mail.getMessage().getSubject());
-        jsonObject.put("body", mail.getMessage().getContent());
+        try {
+            String mailContent = new MimeMessageParser(mail.getMessage()).parse().getHtmlContent();
+            logger.info("initial content: {}", mailContent);
+            mailContent = mailContent.split("<div style=\"color:#a0a8a8;\" id=\"mo-hld-reply\">")[0];
+            logger.info("Processed content: {}", mailContent);
+            jsonObject.put("body", mailContent);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         jsonObject.put("date", mail.getMessage().getSentDate());
 
         String args = jsonObject.toString();
         logger.info("Mail args: {}", args);
-        String response = makeRemoteCall(uri, args);
+        String response = makeRestRemoteCall(uri, args);
         logger.info("API Response: {}", response);
     }
 
-    private String makeRemoteCall(String uri, String args) {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Content-Type","application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(args))
-                .build();
-
-        HttpResponse<String> response = null;
+    private String makeRestRemoteCall(String uri, String args) {
         try {
-            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<String> request = new HttpEntity<>(args, headers);
+            return restTemplate.postForObject(uri, request, String.class);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            return StringUtils.EMPTY;
         }
-        return response.body();
     }
 
     @Override
